78508580f8ee02ba4cd9cfffc0190686baf676c0
Gary Ewan Park <gep13@gep13.co.uk>
2012-10-09 20:02:58 +0100

    Added base Demo 8 Project
    Not going to be much live coding with this one, just an example of how
    we can use some of the base classes of Caliburn.Micro to do Launchers
    and Choosers within a more MVVM friendly way.

---
+++ src/Demo 8 - Launchers and Choosers/CaliburnMicroDemos.Demo8.LaunchersAndChoosers/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CaliburnMicroDemos.Demo8.LaunchersAndChoosers
+{
+    using System.Windows;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
