a135e98b5694dca80333174ceeef0646c8dffa5b
Gary Ewan Park <gep13@gep13.co.uk>
2012-10-09 14:28:50 +0100

    Demo 4 base project created
    Went through all the steps for Demo 1, 2, and 3, ready to start Demo 4.

---
+++ src/Demo 4 - More Conventions and Navigation/CaliburnMicroDemos.Demo4.ConventionsAndNavigation/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,42 @@
+using Caliburn.Micro;
+namespace CaliburnMicroDemos.Demo4.ConventionsAndNavigation.ViewModels
+{
+    public class MainPageViewModel : PropertyChangedBase
+    {
+        private readonly INavigationService navigationService;
+        private bool isNavigationAllowed;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        public bool IsNavigationAllowed
+        {
+            get
+            {
+                return this.isNavigationAllowed;
+            }
+            set
+            {
+                this.isNavigationAllowed = value;
+                this.NotifyOfPropertyChange(() => this.IsNavigationAllowed);
+                this.NotifyOfPropertyChange(() => this.CanGoToSettingsView);
+            }
+        }
+
+        public bool CanGoToSettingsView
+        {
+            get
+            {
+                return this.IsNavigationAllowed;
+            }
+        }
+
+        public void GoToSettingsView()
+        {
+            this.navigationService.UriFor<SettingsViewModel>()
+                .Navigate();
+        }
+    }
+}
