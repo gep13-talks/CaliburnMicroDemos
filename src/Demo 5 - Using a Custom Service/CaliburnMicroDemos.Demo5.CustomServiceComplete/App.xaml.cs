1f0a7021c3b03e41c8bf3ed696f6dbbeec97347e
Gary Ewan Park <gep13@gep13.co.uk>
2013-01-12 23:12:08 +0000

    Created completed version of Demo 5
    Went through all the steps to complete Demo 5 based on what is in the
    wiki article

---
+++ src/Demo 5 - Using a Custom Service/CaliburnMicroDemos.Demo5.CustomServiceComplete/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CaliburnMicroDemos.Demo5.CustomServiceComplete
+{
+    using System.Windows;
+
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Demo 5 - Using a Custom Service/CaliburnMicroDemos.Demo5.CustomServiceComplete/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+namespace CaliburnMicroDemos.Demo5.CustomServiceComplete.ViewModels
+{
+    using CaliburnMicroDemos.Demo5.CustomServiceComplete.Services.Progress;
+
+    public class MainPageViewModel
+    {
+        private IProgressService progressService;
+
+        public MainPageViewModel(IProgressService progressService)
+        {
+            this.progressService = progressService;
+        }
+
+        public void ShowProgressBar()
+        {
+            this.progressService.Show();
+        }
+
+        public void HideProgressBar()
+        {
+            this.progressService.Hide();
+        }
+
+        public void ToggleProgressbar()
+        {
+            this.progressService.Toggle();
+        }
+    }
+}
